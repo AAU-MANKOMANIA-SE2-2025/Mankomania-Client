b17c7d7dbb7fa34b7400d28c2e12d623d4741d33
MauMauChat <lucaan@edu.aau.at>
2025-03-19 23:11:17 +0100

    Update sonarcube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "org.example"
@@ -7,6 +8,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+// Hier in der Kotlin-DSL: "sonarqube", nicht "sonar"
+sonarqube {
+    properties {
+        property("sonar.projectKey", "AAU-MANKOMANIA-SE2-2025_Mankomania-Client")
+        property("sonar.organization", "aau-mankomania-se2-2025")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
 
 dependencies {
